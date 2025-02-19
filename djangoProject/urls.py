30b9e25efcb969f35e5032e9e8f564229d3d2915
hamzaYali <98500914+hamzaYali@users.noreply.github.com>
2025-02-18 21:20:57 -0600

    copy repo

---
+++ djangoProject/urls.py
@@ -0,0 +1,19 @@
+"""
+URLs for the Skills List API
+API allows anyone access to list, create, update, and delete skills
+"""
+from django.contrib import admin
+from django.urls import path, include, re_path
+from api import views
+from django.views.static import serve
+from django.conf import settings
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.skillsList.as_view()),
+    path('api/skillsList/', views.skillsList.as_view()),
+    path('api/skills/', views.skillsListCreate.as_view()),
+    path('api/skills/<int:pk>',
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}), #serve media files when deployed
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}), #serve static files when deployed
+views.skillRetrieveUpdateDestroy.as_view()),
+]

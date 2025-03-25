b00cac671134a350d1021d5474f27050fd0d6912
hamzaYali <98500914+hamzaYali@users.noreply.github.com>
2025-03-24 22:36:37 -0500

    debug enabled

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qcbr@h%!si3czqk0)yv^cg6e6jp9hni%ac=o%)cqafhben#!nc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 

8a9e8910ab4d1c210e08d99b8bbc7599bcba2ca1
hamzaYali <98500914+hamzaYali@users.noreply.github.com>
2025-03-24 22:44:47 -0500

    Update models.py

--- api/models.py
+++ api/models.py
@@ -3,4 +3,4 @@ from django.db import models
 # Data model defining a skill
 class Skill(models.Model):
     skill = models.CharField(max_length=40)
-    description = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)  # Remove the extra quote at the end
